000a8161a2389560eb2752df542db0cd2bcd0bb1
Maxwell Gottesman <maxgman16@yahoo.com>
2022-11-20 20:24:12 -0500

    commit tree 07f9c3d66b00c2de32edb4ebcbd7fb88311bffd2

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let backwards = [];
+  
+  for(let i = 0;i < word.length;++i) {
+    backwards[word.length - 1 - i] = word.toLowerCase()[i];
+  }
+  backwards = backwards.reduce((ac,cv) => ac += cv);
+  return backwards === word.toLowerCase();
 }
 
 /* 
-  Add your pseudocode here
+  Lowercase the string
+  Make a new array called backwards
+  Iterate through the string, putting the current letter into backwards at an index that equals the length minus the iterator
+  reduce backwards into a string
+  return the value of strictly comparing the two strings
 */
 
 /*
@@ -13,13 +23,22 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
+  console.log("Racecar\nExpecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("mMmmM\nExpecting: true");
+  console.log("=>", isPalindrome("mMmmM"));
+
+  console.log("a\nExpecting: true");
+  console.log("=>", isPalindrome("a"));
 
   console.log("");
 
-  console.log("Expecting: false");
+  console.log("Robot\nExpecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("nmnm\nExpecting: false");
+  console.log("=>", isPalindrome("nmnm"));
 }
 
 module.exports = isPalindrome;
